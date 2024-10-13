7e25046242aed3e14dd4cbdc2f4bf9b30ae4bf70
Manimaran Srinivasan <srimaran712@gmail.com>
2024-10-13 19:00:13 +0530

    recommit

--- frontend/recommendationsytem/src/App.js
+++ frontend/recommendationsytem/src/App.js
@@ -11,10 +11,10 @@ function App() {
     <div className="flex">
       <SearchQuery setOpenTab={setOpenTab}/>
      {openTab && <motion.div
-            initial={{ opacity: 0, y: 50 }}   // starting state (faded out and shifted down)
-            animate={{ opacity: 1, y: 0 }}    // animated to (fully visible and at position)
-            exit={{ opacity: 0, y: 50 }}      // exit state (fades out and shifts down)
-            transition={{ duration: 0.5, delay: 0.4 }}  // transition speed and delay
+            initial={{ opacity: 0, y: 50 }}   
+            animate={{ opacity: 1, y: 0 }}    
+            exit={{ opacity: 0, y: 50 }}      
+            transition={{ duration: 0.5, delay: 0.4 }}  
           >  <History  setOpenTab={setOpenTab}/>  </motion.div>}
     </div>
   );
